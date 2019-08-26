fcaab01dbf24cc4b51375d25211347c7f1fbd24d
mmurphy316 <mmurphy316@bloomberg.net>
2019-08-25 21:31:23 -0400

    using iter for stack system

--- src/systems/stack.rs
+++ src/systems/stack.rs
@@ -61,7 +61,8 @@ impl<'a> System<'a> for StackSystem {
                     if input.mouse_button_is_down(MouseButton::Left) {
                         let offset = Vector2::new(cursor.x - prev_point.x, cursor.y - prev_point.y);
                         for (sprite_transform, _stack) in (&mut transformations, &stacks).join() {
-                            sprite_transform.prepend_translation(Vector3::new(offset.x, offset.y, 0.));
+                            sprite_transform
+                                .prepend_translation(Vector3::new(offset.x, offset.y, 0.));
                         }
                         Some(Vector2::new(cursor.x, cursor.y))
                     } else {
@@ -79,39 +80,39 @@ impl<'a> System<'a> for StackSystem {
                     }
                 }
                 None => {
-                    let mut selected: Option<(Entity, Vector3<f32>)> = None;
-                    for (sprite, sprite_transform, card, entity) in
-                        (&sprites, &transformations, &cards, &entities).join()
-                    {
-                        match card.state {
-                            CardState::TableauCovered => continue,
-                            _ => {}
-                        };
-                        if let Some(sprite_sheet) = sheet_storage.get(&sprite.sprite_sheet) {
-                            let sprite = &sprite_sheet.sprites[sprite.sprite_number];
-                            let texture_rect = Rectangle2::new(
-                                -sprite.width,
-                                -sprite.height,
-                                sprite.width,
-                                sprite.height,
-                            ) / 2.;
-                            let global_matrix = *sprite_transform.global_matrix();
-                            let middle = global_matrix * Point4::new(0., 0., 0., 1.);
-                            let transform_rect = texture_rect * global_matrix;
-                            if transform_rect.contains(cursor.x, cursor.y) {
-                                if let Some((_previous, point)) = &mut selected {
-                                    if middle.z < point.z {
-                                        selected =
-                                            Some((entity, *sprite_transform.translation()));
+                    let selected = (&sprites, &transformations, &cards, &entities)
+                        .join()
+                        .filter(
+                            |(sprite, sprite_transform, card, entity)| match card.state {
+                                CardState::TableauCovered => false,
+                                _ => {
+                                    if let Some(sprite_sheet) =
+                                        sheet_storage.get(&sprite.sprite_sheet)
+                                    {
+                                        let sprite = &sprite_sheet.sprites[sprite.sprite_number];
+                                        let texture_rect = Rectangle2::new(
+                                            -sprite.width,
+                                            -sprite.height,
+                                            sprite.width,
+                                            sprite.height,
+                                        ) / 2.;
+                                        let global_matrix = *sprite_transform.global_matrix();
+                                        let transform_rect = texture_rect * global_matrix;
+                                        transform_rect.contains(cursor.x, cursor.y)
+                                    } else {
+                                        false
                                     }
-                                } else {
-                                    selected = Some((entity, *sprite_transform.translation()));
                                 }
-                            }
-                        }
-                    }
-                    if let Some((entity, point)) = &mut selected {
-                        stacks.insert(*entity, StackComponent::new(*point));
+                            },
+                        )
+                        .map(|(sprite, sprite_transform, card, entity)| {
+                            (entity, *sprite_transform.translation())
+                        })
+                        .min_by(|(entity1, transform1), (entity2, transform2)| {
+                            transform1.z.partial_cmp(&transform2.z).unwrap()
+                        });
+                    if let Some((entity, point)) = selected {
+                        stacks.insert(entity, StackComponent::new(point));
                     }
                     Some(Vector2::new(cursor.x, cursor.y))
                 }
