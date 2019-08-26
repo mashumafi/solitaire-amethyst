cd2c9a585b6ced43c256734bac8bd1930f076339
mmurphy316 <mmurphy316@bloomberg.net>
2019-08-25 21:09:32 -0400

    refactor stack system

--- src/systems/stack.rs
+++ src/systems/stack.rs
@@ -55,67 +55,67 @@ impl<'a> System<'a> for StackSystem {
             entities,
         ): Self::SystemData,
     ) {
-        let was_pressed = self.point.is_some();
-        let is_pressed = input.mouse_button_is_down(MouseButton::Left);
-
-        if !is_pressed {
-            self.point = None;
-            if let Some((sprite_transform, stack, entity)) =
-                (&mut transformations, &mut stacks, &entities).join().next()
-            {
-                sprite_transform.set_translation_xyz(
-                    stack.original.x,
-                    stack.original.y,
-                    stack.original.z,
-                );
-                stacks.remove(entity);
-            }
-            return;
-        }
-
         if let Some(cursor) = screen_to_world(&cameras, &input, &dimension, &transformations) {
-            if was_pressed {
-                let last_cursor = self.point.unwrap();
-                let offset = Vector2::new(cursor.x - last_cursor.x, cursor.y - last_cursor.y);
-                for (sprite_transform, _stack) in (&mut transformations, &stacks).join() {
-                    sprite_transform.prepend_translation(Vector3::new(offset.x, offset.y, 0.));
+            self.point = match self.point {
+                Some(prev_point) => {
+                    if input.mouse_button_is_down(MouseButton::Left) {
+                        let offset = Vector2::new(cursor.x - prev_point.x, cursor.y - prev_point.y);
+                        for (sprite_transform, _stack) in (&mut transformations, &stacks).join() {
+                            sprite_transform.prepend_translation(Vector3::new(offset.x, offset.y, 0.));
+                        }
+                        Some(Vector2::new(cursor.x, cursor.y))
+                    } else {
+                        if let Some((sprite_transform, stack, entity)) =
+                            (&mut transformations, &mut stacks, &entities).join().next()
+                        {
+                            sprite_transform.set_translation_xyz(
+                                stack.original.x,
+                                stack.original.y,
+                                stack.original.z,
+                            );
+                            stacks.remove(entity);
+                        }
+                        None
+                    }
                 }
-            } else {
-                let mut selected: Option<(Entity, Vector3<f32>)> = None;
-                for (sprite, sprite_transform, card, entity) in
-                    (&sprites, &transformations, &cards, &entities).join()
-                {
-                    match card.state {
-                        CardState::TableauCovered => continue,
-                        _ => {}
-                    };
-                    if let Some(sprite_sheet) = sheet_storage.get(&sprite.sprite_sheet) {
-                        let sprite = &sprite_sheet.sprites[sprite.sprite_number];
-                        let texture_rect = Rectangle2::new(
-                            -sprite.width,
-                            -sprite.height,
-                            sprite.width,
-                            sprite.height,
-                        ) / 2.;
-                        let global_matrix = *sprite_transform.global_matrix();
-                        let middle = global_matrix * Point4::new(0., 0., 0., 1.);
-                        let transform_rect = texture_rect * global_matrix;
-                        if transform_rect.contains(cursor.x, cursor.y) {
-                            if let Some((_previous, point)) = &mut selected {
-                                if middle.z < point.z {
+                None => {
+                    let mut selected: Option<(Entity, Vector3<f32>)> = None;
+                    for (sprite, sprite_transform, card, entity) in
+                        (&sprites, &transformations, &cards, &entities).join()
+                    {
+                        match card.state {
+                            CardState::TableauCovered => continue,
+                            _ => {}
+                        };
+                        if let Some(sprite_sheet) = sheet_storage.get(&sprite.sprite_sheet) {
+                            let sprite = &sprite_sheet.sprites[sprite.sprite_number];
+                            let texture_rect = Rectangle2::new(
+                                -sprite.width,
+                                -sprite.height,
+                                sprite.width,
+                                sprite.height,
+                            ) / 2.;
+                            let global_matrix = *sprite_transform.global_matrix();
+                            let middle = global_matrix * Point4::new(0., 0., 0., 1.);
+                            let transform_rect = texture_rect * global_matrix;
+                            if transform_rect.contains(cursor.x, cursor.y) {
+                                if let Some((_previous, point)) = &mut selected {
+                                    if middle.z < point.z {
+                                        selected =
+                                            Some((entity, *sprite_transform.translation()));
+                                    }
+                                } else {
                                     selected = Some((entity, *sprite_transform.translation()));
                                 }
-                            } else {
-                                selected = Some((entity, *sprite_transform.translation()));
                             }
                         }
                     }
-                }
-                if let Some((entity, point)) = &mut selected {
-                    stacks.insert(*entity, StackComponent::new(*point));
+                    if let Some((entity, point)) = &mut selected {
+                        stacks.insert(*entity, StackComponent::new(*point));
+                    }
+                    Some(Vector2::new(cursor.x, cursor.y))
                 }
             }
-            self.point = Some(Vector2::new(cursor.x, cursor.y));
         }
     }
 }
